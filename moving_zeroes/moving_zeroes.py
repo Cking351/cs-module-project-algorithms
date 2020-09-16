25b4609bd38c5a002597cbc71a085bb193015b55
Chris King <cking5836@gmail.com>
2020-09-16 15:46:31 -0400

    Moving zeros done

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,23 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
+    new_arr = []
+    zero_arr = []
+    arr.sort()
+    for i in arr:
+        if i > 0:
+            new_arr.append(i)
+        elif i < 0:
+            new_arr.append(i)
+        elif i == 0:
+            zero_arr.append(i)
 
-    pass
+    new_arr.extend(zero_arr)
+    return new_arr
 
 
 if __name__ == '__main__':
